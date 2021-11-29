d9e0a032da34a37aed704402f9a2ee8c2de0215e
wfercanas <fernando.canas@outlook.com>
2021-11-29 15:30:38 -0500

    add/ tabindex for projects carousel

--- index.js
+++ index.js
@@ -1,63 +1,91 @@
 window.onload = () => {
-  document.querySelector(".arrow-right").addEventListener("click", clickRight);
-  document.querySelector(".arrow-left").addEventListener("click", clickLeft);
+  document.querySelector('.arrow-right').addEventListener('click', clickRight);
+  document.querySelector('.arrow-left').addEventListener('click', clickLeft);
   document
-    .querySelector(".send-button")
-    .addEventListener("click", showNotification);
-  document.querySelectorAll(".project").forEach(element => {
-    element.addEventListener("click", e => openModal(e));
+    .querySelector('.send-button')
+    .addEventListener('click', showNotification);
+  document.querySelectorAll('.project').forEach((element) => {
+    element.addEventListener('click', (e) => openModal(e));
   });
-  document.body.addEventListener("click", e => closeModal(e));
+  document.body.addEventListener('click', (e) => closeModal(e));
 };
 
 /** Esta funcion se llama cuando la persona hace click en la fecha derecha del carousel para navegar a la derecha */
 function clickRight() {
   const currentLeft = parseInt(
-    getComputedStyle(document.querySelector(".project-container")).left,
+    getComputedStyle(document.querySelector('.project-container')).left,
     10
   );
-  if (currentLeft < -270) { //si el valor de izquierda es menor a -270, para de mover el contenido
+  if (currentLeft < -270) {
+    //si el valor de izquierda es menor a -270, para de mover el contenido
     return;
   }
   let newValue = currentLeft - 270; //270 toma en cuenta el tamaño de la imagen mas sus margines
-  document.querySelector(".project-container").style.left = `${newValue}px`;
+  document.querySelector('.project-container').style.left = `${newValue}px`;
+
+  switch (newValue) {
+    case -270:
+      document.querySelector('.project1').setAttribute('tabindex', '-1');
+      document.querySelector('.project4').removeAttribute('tabindex');
+      break;
+    case -540:
+      document.querySelector('.project2').setAttribute('tabindex', '-1');
+      document.querySelector('.project5').removeAttribute('tabindex');
+      break;
+    default:
+      break;
+  }
 }
 
 /** Esta funcion se llama cuando la persona hace click en la fecha izquierda del carousel para navegar a la izquierda */
 function clickLeft() {
   const currentLeft = parseInt(
-    getComputedStyle(document.querySelector(".project-container")).left,
+    getComputedStyle(document.querySelector('.project-container')).left,
     10
   );
-  if (currentLeft === 0) { //si el valor de izquiera es 0, retornar para no seguir movierno el contenido
+  if (currentLeft === 0) {
+    //si el valor de izquiera es 0, retornar para no seguir movierno el contenido
     return;
   }
   let newValue = currentLeft + 270;
-  document.querySelector(".project-container").style.left = `${newValue}px`;
+  document.querySelector('.project-container').style.left = `${newValue}px`;
+
+  switch (newValue) {
+    case -270:
+      document.querySelector('.project5').setAttribute('tabindex', '-1');
+      document.querySelector('.project2').removeAttribute('tabindex');
+      break;
+    case 0:
+      document.querySelector('.project4').setAttribute('tabindex', '-1');
+      document.querySelector('.project1').removeAttribute('tabindex');
+      break;
+    default:
+      break;
+  }
 }
 
 /** Esta funcion se llama cuando la persona hace click en el boton de enviar del formulario de contacto */
 function showNotification() {
-  document.querySelector(".notification").style.display = "flex";
-  setTimeout(function() {
-    document.querySelector(".notification").style.display = "none";
+  document.querySelector('.notification').style.display = 'flex';
+  setTimeout(function () {
+    document.querySelector('.notification').style.display = 'none';
   }, 3000);
 }
 
 /** Esta funcion se llama cuando la persona hace click en cualquier porjecto del carousel */
 function openModal(e) {
-  document.querySelector(".modal-container").style.display = "flex";
+  document.querySelector('.modal-container').style.display = 'flex';
 }
 
 /** Esta funcion se llama para cerrar el modal */
 function closeModal(e) {
   // si el click occurio dentro del las imagenes del carousel o dentro del modal, no se cierra el modal
   if (
-    e.target.className.includes("project") ||
-    e.target.className === "modal"
+    e.target.className.includes('project') ||
+    e.target.className === 'modal'
   ) {
     return;
   } else {
-    document.querySelector(".modal-container").style.display = "none";
+    document.querySelector('.modal-container').style.display = 'none';
   }
 }
